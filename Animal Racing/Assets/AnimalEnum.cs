21ad220d6d13578a80fff775c0ce82616667f82e
ryanakann <ryanakann@gmail.com>
2020-07-10 15:34:06 -0400

    Initial commit and ES3 import

---
+++ Animal Racing/Assets/AnimalEnum.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//All valid animal types
+public enum AnimalEnum
+{
+    chicken, 
+    pig,
+    cow,
+    sheep,
+}
